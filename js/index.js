2cd817e864c90e6fccd2ecde8e883ed96c2050da
Sarbajit Acharjee <sarbajitacharjee712@gmail.com>
2024-08-25 01:47:57 +0530

    changes

--- js/index.js
+++ js/index.js
@@ -11,14 +11,14 @@ if (hamburer) {
 const popup = document.querySelector(".popup");
 const closePopup = document.querySelector(".popup-close");
 
-if (popup) {
-  closePopup.addEventListener("click", () => {
-    popup.classList.add("hide-popup");
-  });
+// if (popup) {
+//   closePopup.addEventListener("click", () => {
+//     popup.classList.add("hide-popup");
+//   });
 
-  window.addEventListener("load", () => {
-    setTimeout(() => {
-      popup.classList.remove("hide-popup");
-    }, 1000);
-  });
-}
+//   window.addEventListener("load", () => {
+//     setTimeout(() => {
+//       popup.classList.remove("hide-popup");
+//     }, 1000);
+//   });
+// }
